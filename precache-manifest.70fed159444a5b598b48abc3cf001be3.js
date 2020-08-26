1af996fb807e9d0138f26d41a9616e422a98d4b8
aabdelhy <aabdelhy@unitedofoq.com>
2020-08-26 14:47:04 +0200

    Updates

---
+++ precache-manifest.70fed159444a5b598b48abc3cf001be3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3cbb965c7f889104ac9b4c7771d309f",
+    "url": "/ofoq-login/index.html"
+  },
+  {
+    "revision": "998e8388d6160c98304a",
+    "url": "/ofoq-login/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "18b142eb47f26ee0fb64",
+    "url": "/ofoq-login/static/js/2.2b6fb7c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ofoq-login/static/js/2.2b6fb7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998e8388d6160c98304a",
+    "url": "/ofoq-login/static/js/main.792d00cc.chunk.js"
+  },
+  {
+    "revision": "eaec50a5b91439e7a93b",
+    "url": "/ofoq-login/static/js/runtime-main.2087bde3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ofoq-login/static/media/logo.5d5d9eef.svg"
+  }
+]);
